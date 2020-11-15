f57ed119731bd1240bf8de7a2d6f5ed8f1ff680b
Diego Alvis <diegoalvispal@gmail.com>
2020-11-15 15:16:51 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bec2873a1af415b5e085c471cf86536c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bec416bd-b6a3-464f-a3b6-3728f6898292'
+};
